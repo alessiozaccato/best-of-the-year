6a0bcbe16052c6db90ed8d00d8be990719a8532a
alessiozaccato <alessiozaccato@gmail.com>
2025-05-16 16:56:12 +0200

    Add Bootstrap dependency, refactor controllers, and update templates for improved UI

--- src/main/java/com/java/bestoftheyear/best_of_the_year/controllers/MovieController.java
+++ src/main/java/com/java/bestoftheyear/best_of_the_year/controllers/MovieController.java
@@ -9,24 +9,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.java.bestoftheyear.best_of_the_year.classess.Movie;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
 
 @Controller
 @RequestMapping("/")
 public class MovieController {
 
-    private List <Movie> getBestMovies(){
-        List <Movie> moviesList = new ArrayList<>();
+    private List<Movie> getBestMovies() {
+        List<Movie> moviesList = new ArrayList<>();
         moviesList.add(new Movie("Il Padrino", 1));
         moviesList.add(new Movie("Inception", 2));
         moviesList.add(new Movie("Pulp Fiction", 3));
         return moviesList;
     }
-    
-    
+
     @GetMapping("/movies")
     public String getMovieList(Model model) {
         model.addAttribute("movies", getBestMovies());
@@ -34,14 +31,14 @@ public class MovieController {
     }
 
     @GetMapping("/movies/{id}")
-    public String getMovieById(Model model,@PathVariable int id) {
+    public String getMovieById(Model model, @PathVariable int id) {
         for (Movie movie : getBestMovies()) {
-            if (movie.getId()==id) {
+            if (movie.getId() == id) {
                 model.addAttribute(movie);
-            } 
+                break;
+            }
         }
-        return "pageById";
+        return "movieById";
     }
-    
 
 }
--- src/main/java/com/java/bestoftheyear/best_of_the_year/controllers/SongController.java
+++ src/main/java/com/java/bestoftheyear/best_of_the_year/controllers/SongController.java
@@ -11,14 +11,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.java.bestoftheyear.best_of_the_year.classess.Song;
 
-import jakarta.websocket.server.PathParam;
-
 @Controller
 @RequestMapping("/")
 public class SongController {
 
     private List<Song> getBestSong() {
-        List <Song> songsList = new ArrayList<> ();
+        List<Song> songsList = new ArrayList<>();
         songsList.add(new Song("Bohemian Rhapsody", 1));
         songsList.add(new Song("Imagine", 2));
         songsList.add(new Song("Smells Like Teen Spirit", 3));
@@ -32,14 +30,14 @@ public class SongController {
     }
 
     @GetMapping("/songs/{id}")
-    public String getSongById(Model model,@PathVariable int id) {
-        for ( Song song : getBestSong()) {
-            if (song.getId()==id) {
+    public String getSongById(Model model, @PathVariable int id) {
+        for (Song song : getBestSong()) {
+            if (song.getId() == id) {
                 model.addAttribute(song);
+                break;
             }
         }
         return "songById";
     }
-
 
 }
